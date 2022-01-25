6e925a10535f72ab4c25b70fe75edbbefa710c6a
luisdavidld551 <luisdavidld551@gmail.com>
2022-01-25 16:44:53 -0500

    se agrega dependencia nodemon y comentarios

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 //const mysql = require('mysql');
 
 const servidor = express();
-servidor.set('port',process.env.port || 3000);
+servidor.set('port',process.env.port || 4000);
 
+//rutas
+servidor.get('/',(req, res) => {
+    res.send("Bienvenido");
+})
+
+//Server running
 servidor.listen(servidor.get('port'), () => {
     console.log('Server on port',servidor.get('port'));
 });
